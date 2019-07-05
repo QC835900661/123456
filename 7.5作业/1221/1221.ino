434faeba017394d95f98edd8cd229e43ca282308
QC835900661 <52504127+QC835900661@users.noreply.github.com>
2019-07-05 23:56:33 +0800

    Add files via upload

---
+++ 7.5作业/1221/1221.ino
@@ -0,0 +1,50 @@
+    
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  
+  pinMode(5, OUTPUT);
+
+  
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);//dsafadfaqfs
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);//dsgsgsadaf
+}
+
+int counter = 6;
+int n;
+
+void loop()
+{
+  if (Serial.available() > 0)
+  {
+    int i, number;
+    number = Serial.read() - '0';
+    if  (counter == 10)
+    {
+      for (i =  6; i < 10; i++) digitalWrite(i, LOW);
+      counter = 6;
+    }
+    for (i = 2; i <= 5; i++)
+    {
+      if (number % 2 == 1)//fafdadfsaf
+      {
+        digitalWrite(i, HIGH);
+      } else {
+        digitalWrite(i, LOW);
+      }
+
+//fdsafdafdadfa
+      
+      number = number / 2;
+    }
+    digitalWrite(counter, HIGH);
+    counter ++;
+  }
+}
